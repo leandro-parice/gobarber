96855277f421a633b8ef8737a24e9cda74067196
Leandro Parice <contato@leandroparice.com.br>
2019-07-13 18:17:01 -0300

    criado loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Server' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Leandro César Parice',
+    email: 'contato@leandroparice.com.br',
+    password_hash: '123456',
+    provider: false,
+  });
+
+  return res.json(user);
+});
 
 export default routes;
